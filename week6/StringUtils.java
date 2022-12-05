ea0ebe6fb1fc9116fa456fcbfa1aeb6e8af6586c
Melisa Geca <melisa.geca@stu.ibu.edu.ba>
2022-12-05 15:01:25 +0100

    Lab 6

---
+++ week6/StringUtils.java
@@ -0,0 +1,16 @@
+public class StringUtils {
+    public static boolean included(String word, String searched){
+
+        word = word.trim();
+        searched = searched.trim();
+        word = word.toLowerCase();
+        searched = searched.toLowerCase();
+        if(word.contains(searched)){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+    }
+}
